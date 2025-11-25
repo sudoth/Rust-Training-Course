f0a2c2aeabe2a5f867f32e3cfc9d36b7700423d3
sudoth <kotelnikov.iv@phystech.edu>
2025-11-25 14:24:44 +0300

    Added c9 tasks

--- src/tasks/c9_smart_pointers.rs
+++ src/tasks/c9_smart_pointers.rs
@@ -20,7 +20,36 @@ use std::rc::Rc;
 //
 // Use `Box` if needed
 
-// IMPLEMENT HERE:
+pub struct BinaryTreeNode {
+    value: i32,
+    left_child: Option<Box<BinaryTreeNode>>,
+    right_child: Option<Box<BinaryTreeNode>>,
+}
+
+impl BinaryTreeNode {
+    pub fn new(value: i32) -> Self {
+        BinaryTreeNode {
+            value,
+            left_child: None,
+            right_child: None,
+        }
+    }
+
+    pub fn with_children(value: i32, left_child: BinaryTreeNode, right_child: BinaryTreeNode) -> Self {
+        BinaryTreeNode {
+            value,
+            left_child: Some(Box::new(left_child)),
+            right_child: Some(Box::new(right_child)),
+        }
+    }
+
+    pub fn sum(&self) -> i32 {
+        let left_sum = self.left_child.as_ref().map_or(0, |node| node.sum());
+        let right_sum = self.right_child.as_ref().map_or(0, |node| node.sum());
+
+        self.value + left_sum + right_sum
+    }
+}
 
 // Rc
 // ================================================================================================
@@ -33,19 +62,95 @@ use std::rc::Rc;
 // Implement methods:
 // - `new(name: &str) -> Self` which creates a new package with provided name and without any
 //   dependencies.
-// - `with_dependencies(name: &str, dependencies: Vec<Package>) -> Self` which creates a new package
+// - `with_dependencies(name: &str, dependencies: Vec<Rc<Package>>) -> Self` which creates a new package
 //   with provided name and dependencies.
-// - `list_dependencies(package: Package) -> Vec<String>` which return a vector of all dependencies
+// - `list_dependencies(package: &Rc<Package>) -> Vec<String>` which return a vector of all dependencies
 //   of this package (including all recursive dependencies).
 //
 // Write a test which will reuse the created Packages in several other Packages as dependencies.
 // Use `Rc` in the `Package` struct where needed to avoid deep clone.
 
-// IMPLEMENT HERE:
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Package {
+    pub name: String,
+    pub dependencies: Vec<Rc<Package>>,
+}
+
+impl Package {
+    pub fn new(name: &str) -> Rc<Self> {
+        Rc::new(Package {
+            name: name.to_string(),
+            dependencies: Vec::new(),
+        })
+    }
+
+    pub fn with_dependencies(name: &str, dependencies: Vec<Rc<Package>>) -> Rc<Self> {
+        Rc::new(Package {
+            name: name.to_string(),
+            dependencies,
+        })
+    }
+
+    pub fn list_dependencies(package: &Rc<Package>) -> Vec<String> {
+        let mut all_deps: Vec<String> = Vec::new();
+        let mut visited: std::collections::HashSet<String> = std::collections::HashSet::new();
+
+        fn traverse(pkg: &Rc<Package>, all_deps: &mut Vec<String>, visited: &mut std::collections::HashSet<String>) {
+            for dep in &pkg.dependencies {
+                if !visited.contains(&dep.name) {
+                    visited.insert(dep.name.clone());
+                    all_deps.push(dep.name.clone());
+                    traverse(dep, all_deps, visited);
+                }
+            }
+        }
+
+        traverse(package, &mut all_deps, &mut visited);
+        all_deps.sort();
+        all_deps
+    }
+}
+
 
 #[test]
 fn test_list_dependencies() {
-    // IMPLEMENT HERE:
+    let lib_a = Package::new("LibA");
+    let lib_b = Package::new("LibB");
+    
+    let core_pkg = Package::with_dependencies(
+        "Core", 
+        vec![Rc::clone(&lib_a), Rc::clone(&lib_b)]
+    );
+
+    let ui_pkg = Package::with_dependencies(
+        "UI", 
+        vec![Rc::clone(&lib_b)]
+    );
+
+    let main_app = Package::with_dependencies(
+        "MainApp",
+        vec![Rc::clone(&core_pkg), Rc::clone(&ui_pkg), Package::new("Utils")]
+    );
+
+    let dependencies = Package::list_dependencies(&main_app);
+    let mut expected = vec!["Core".to_string(), "UI".to_string(), "Utils".to_string(), "LibA".to_string(), "LibB".to_string()];
+    expected.sort();
+
+    assert_eq!(dependencies.len(), 5);
+
+    let mut expected_deps = vec![
+        "Core".to_string(), 
+        "UI".to_string(), 
+        "Utils".to_string(), 
+        "LibA".to_string(), 
+        "LibB".to_string()
+    ];
+    expected_deps.sort();
+
+    assert_eq!(dependencies, expected_deps);
+    
+    assert_eq!(Rc::strong_count(&lib_b), 3);
+    assert_eq!(Rc::strong_count(&lib_a), 2);
 }
 
 // RefCell
@@ -58,21 +163,22 @@ fn test_list_dependencies() {
 // Implement `new() -> Self` constructor, `increment(&self)` and `get(&self) -> i32` methods.
 // Use `RefCell` where needed.
 
-// IMPLEMENT HERE:
 pub struct SharedCounter {
-    value: i32,
+    value: RefCell<i32>,
 }
 
 impl SharedCounter {
     pub fn new() -> Self {
-        !unimplemented!()
+        SharedCounter {
+            value: RefCell::new(0),
+        }
     }
 
     pub fn increment(&self) {
-        !unimplemented!()
+        *self.value.borrow_mut() += 1;
     }
 
     pub fn get(&self) -> i32 {
-        !unimplemented!()
+        *self.value.borrow()
     }
 }
